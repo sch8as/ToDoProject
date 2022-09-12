491825ec91e30b792b19a9e4bbd9a1a42d9e3c54
Sch9AS <sch9as@gmail.com>
2022-09-12 08:37:39 +0400

    Delete welcome page

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ Route::group(['middleware' => ['auth']], function($router) {
     Route::resource('tasks', TaskController::class)->only(['index', 'store', 'destroy']);
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
